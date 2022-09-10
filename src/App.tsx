d491340a3115b145f3b4b3aaf3ff6a02438d78b8
Brandon Branson <brandonkbranson@gmail.com>
2022-09-09 22:09:26 -0400

    Trying to commit this, added my name to the app

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Brandon was here.
+                automatically reload. Brandon was here. Testing this.
             </p>
         </div>
     );
